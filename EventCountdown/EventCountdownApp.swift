52817fdb4ad9941ee51d72b048c9e75716bdfd9d
maddy392 <maddy392@gmail.com>
2024-07-04 02:36:38 -0500

    Initial Commit

---
+++ EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by Madhu Babu Adiki on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
